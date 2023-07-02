288f3e3fcfa18c9a4f83765d68e742d49711918d
dingzu <dzwangyihan@gmail.com>
2023-07-02 15:21:12 +0800

    安装 axios

---
+++ src/components/FingerGame.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>这是一个猜拳游戏</div>
+</template>
+  
+<script lang="ts">
+import { defineComponent, inject } from "vue";
+export default defineComponent({
+  name: "FingerGame",
+  mounted() {
+    this.$axios.get("/barry").then((res) => {});
+  },
+});
+</script>
+<style lang="stylus"></style>
+  
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@vue/runtime-core'
+import { AxiosInstance } from "axios";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(router).mount('#app');
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        $axios: AxiosInstance;
+    }
+}
