2a213e8f8cea6b7503f05f8528dc8e797d45b0c4
wangyihan <wangyihan@kuaishou.com>
2023-07-04 13:21:07 +0800

    更新支持 vercel 代理

--- src/components/FingerGame.vue
+++ src/components/FingerGame.vue
@@ -1,26 +1,26 @@
 <template>
   <div>这是一个猜拳游戏</div>
-  <div>你是：{{player}}</div>
+  <div>你是：{{ player }}</div>
   <div>你想要出什么?</div>
   <div>
-    <div @click="sendChoose('scissors')" class="button">剪刀</div>
-    <div @click="sendChoose('stone')" class="button">石头</div>
-    <div @click="sendChoose('cloth')" class="button">布</div>
+    <div @click="sendChoose('scissors')" :class="['button', isChoose('scissors')]">剪刀</div>
+    <div @click="sendChoose('stone')" :class="['button', isChoose('stone')]">石头</div>
+    <div @click="sendChoose('cloth')" :class="['button', isChoose('cloth')]">布</div>
   </div>
-  <div>你的对手是：{{otherPlayer}}</div>
-  <div>他出了: {{bothPlayer[otherPlayer]}}</div>
+  <div>你的对手是：{{ otherPlayer }}</div>
+  <div>他出了: {{ bothPlayer[otherPlayer] }}</div>
 </template>
 
 <script lang='ts' setup>
-import {fingerApi,Choose,ChooseBoth,fingertype} from "@/api/fingerApi";
-import { onMounted,ref } from "vue";
-import {useRouter } from 'vue-router'
+import { fingerApi, Choose, ChooseBoth, fingertype } from "@/api/fingerApi";
+import { onMounted, ref } from "vue";
+import { useRouter } from 'vue-router'
 const http = new fingerApi();
 const router = useRouter()
 const player = router.currentRoute.value.params.player.toString()
-const otherPlayer = player=='player1'? 'player2':'player1'
+const otherPlayer = player == 'player1' ? 'player2' : 'player1'
 
-const turn:Number = 0
+const turn: Number = 0
 
 let bothPlayer = ref<ChooseBoth>({
   turn: turn,
@@ -28,34 +28,44 @@ let bothPlayer = ref<ChooseBoth>({
   player2: "wait"
 })
 
-let playerData:Choose = {
+let playerData: Choose = {
   turn: turn,
   player: player === 'player1' ? 'player1' : player === 'player2' ? 'player2' : undefined,
   choose: "wait"
 };
 
+function isChoose(choose: fingertype) {
+  if (choose == playerData.choose) {
+    return 'active'
+  } else {
+    return ''
+  }
+}
+
 function getChoose() {
   http
     .getChoose()
     .then((result) => {
-        bothPlayer.value = result
-        getChoose()
+      bothPlayer.value = result
+      playerData.choose = result[player]
+      setTimeout(() => { getChoose() }, 1000)
     })
     .catch((err) => {
       console.log(err.message, "err");
     });
 }
 
-function sendChoose(chose:fingertype) {
-  playerData.choose = chose
+function sendChoose(choose: fingertype) {
+  console.log('点了')
+  playerData.choose = choose
   http
     .sendChoose(playerData)
     .then((result) => {
-      if (result.player2 == "no" || result.player1 == "no") {
-        getChoose()
+      if (result.player2 == "wait" || result.player1 == "wait") {
+        setTimeout(() => { getChoose() }, 1000)
       }
     })
-    .catch((err) => {});
+    .catch((err) => { });
 }
 
 
@@ -72,5 +82,8 @@ onMounted(() => {
   margin 20px 0
   &:hover
     cursor pointer
+  &.active
+    background-color #000
+    color #fff
 </style>
   
